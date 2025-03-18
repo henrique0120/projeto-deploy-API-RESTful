8c1c71ccd08702962b04c6181b3a7b2b29d3c271
henrique0120 <henrique.sl959@hotmail.com>
2025-03-18 11:55:27 -0300

    Implementação da camada de domínio

---
+++ src/main/java/com/henrique/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.henrique.domain.repository;
+
+import com.henrique.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
